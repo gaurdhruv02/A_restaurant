4c1481f1e5c66398e355dd5a36aa53e0d63f90a0
Dhruv Gaur <gaurdhruv1997@gmail.com>
2018-06-08 12:26:05 +0530

    header and footer

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
+
 
 @Component({
   selector: 'app-menu',
@@ -10,13 +11,15 @@ import { DISHES } from '../shared/dishes';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
 
-  selectedDish : Dish;
-  
-  constructor() { }
+  dishes: Dish[];
+
+  selectedDish: Dish;
+
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish) {
